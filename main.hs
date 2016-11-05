5fbba80e1d8313e912be08aad3ab04854be7ad31
Lane Miles <lmiles1234@gmail.com>
2016-11-05 15:01:50 -0700

    i dont remember

--- main.hs
+++ main.hs
@@ -1,13 +1,27 @@
 module Main where
 import Interp
 import Parse
+import System.IO
+import System.Environment
 
 
 main :: IO ()
 main = do
-    s <- getContents
-    let ast = parseInput s
-    let res = evalAst ast
-    print res
-
-
+        args <- getArgs
+        if (length args) == 1 && (head args) /= "-"
+            then
+                do
+                    programStr <- readFile (head args)
+                    let ast = parseInput programStr
+                    let res = evalAst ast
+                    print res
+            else
+                if (length args == 0) || (length args == 1 && (head args == "-"))
+                    then 
+                        do 
+                            programStr <- getContents
+                            let ast = parseInput programStr
+                            let res = evalAst ast
+                            print res
+                    else
+                        putStr "MEH"
